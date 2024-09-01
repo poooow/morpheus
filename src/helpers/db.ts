1250f366cc3d7f9cb1c3fa482190ffb8e8637717
pow <poooow@gmail.com>
2024-09-01 10:48:12 +0200

    Linter

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -15,17 +15,17 @@ export async function fetchDrugList() {
 }
 
 function parseCSV(csvText: string) {
-  const rows = csvText.split(/\r?\n/);
-  const headers = rows[0].split(',');
-  const data = [];
+  const rows = csvText.split(/\r?\n/)
+  const headers = rows[0].split(',')
+  const data = []
 
   for (let i = 1; i < rows.length; i++) {
-      const rowData = rows[i].split(',');
-      const rowObject: {[key: string]: string} = {};
-      for (let j = 0; j < headers.length; j++) {
-          rowObject[headers[j]] = rowData[j];
-      }
-      data.push(rowObject);
+    const rowData = rows[i].split(',')
+    const rowObject: { [key: string]: string } = {}
+    for (let j = 0; j < headers.length; j++) {
+      rowObject[headers[j]] = rowData[j]
+    }
+    data.push(rowObject)
   }
-  return data;
+  return data
 }
