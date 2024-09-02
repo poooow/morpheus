29837037583004cc96bff5ab1a02e6d35409acee
pow <poooow@gmail.com>
2024-09-02 11:24:37 +0200

    Typo

--- src/componets/inputSelectorSteps/index.tsx
+++ src/componets/inputSelectorSteps/index.tsx
@@ -7,21 +7,21 @@ interface Props {
 }
 
 export default function InputSelectorSwipe({ count, setCount }: Props) {
-  const [cypher, setCypher] = useState<number>(0)
-  const [firstCypher, setFirstCypher] = useState<number>(0)
+  const [power, setPower] = useState<number>(0)
+  const [firstPower, setFirstPower] = useState<number>(0)
 
 
   const handleButtonClick = (count: number) => {
-    if (cypher === 0) setFirstCypher(count)
-    else setCount(firstCypher * 10 + count)
-    setCypher((cypher + 1) % 2)
+    if (power === 0) setFirstPower(count)
+    else setCount(firstPower * 10 + count)
+    setPower((power + 1) % 2)
   }
 
   return (
     <S.Container>
       {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((count) =>
         <S.Button key={count} onClick={() => handleButtonClick(count)}>
-          {cypher === 0 ? `${count}_` : `${firstCypher === 0 ? '' : firstCypher}${count}`}
+          {power === 0 ? `${count}_` : `${firstPower === 0 ? '' : firstPower}${count}`}
         </S.Button>
       )}
     </S.Container>
