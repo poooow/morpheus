9f64497659997fa12c3ba6258c2c65fae5911dab
pow <poooow@gmail.com>
2024-09-07 00:28:13 +0200

    Fix share img path

--- src/components/shareModal/index.tsx
+++ src/components/shareModal/index.tsx
@@ -1,3 +1,4 @@
+import share from './share.svg';
 import * as S from './styles'
 
 type Props = {
@@ -8,7 +9,7 @@ export default function ShareModal({ hide }: Props) {
   return (
     <S.Overlay onClick={hide}>
       <S.Container>
-        <img src="/share.svg" alt="Share QR Code" width="500" height="500" />
+        <img src={share} alt="Share QR Code" width="500" height="500" />
         <div>https://kkns.eu/morpheus</div>
       </S.Container>
     </S.Overlay>
